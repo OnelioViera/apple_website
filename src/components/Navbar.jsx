2e291d120b4bf2670538d5d1cdeffa82453541cc
Onelio Viera <ojvwebdesign@gmail.com>
2024-07-31 19:26:36 -0600

    chore: Add initial project files and structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { appleImg } from '../utils'
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="">
+        <img
+          src={appleImg}
+        />
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
